883f66961070e85244867def185488164fcc1eb0
Mohit241008 <97454537+Mohit241008@users.noreply.github.com>
2022-01-11 14:46:28 +0530

    Add files via upload

---
+++ HackerRank/EASY/5_for_loop/5_for_loop.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n,l;
+
+	cin>>n>>l;
+
+	for (int i=n; i < 10; n++)
+	{
+		switch(n){
+
+		case 1:cout<<"one";break;
+		case 2:cout<<"two";break;
+		case 3:cout<<"three";break;
+		case 4:cout<<"four";break;
+		case 5:cout<<"five";break;
+		case 6:cout<<"six";break;
+		case 7:cout<<"seven";break;
+		case 8:cout<<"eight";break;
+		default:cout<<"nine";break;
+	}
+	}
+}
